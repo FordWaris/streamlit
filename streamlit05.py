5c04480fa5c3a62a2d1abfdb0b86a4f7cafbd6df
FordWaris <145241799+FordWaris@users.noreply.github.com>
2023-09-17 11:32:00 +0700

    Add files via upload

---
+++ streamlit05.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title('การทดสอบการเขียนเว็บด้วย Streamlit')
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.header("วริศ ศรีตงกิม")
+with col1:
+    #st.header("Versicolor")
+    st.image("./pic/images.jpg")
+
+st.header("วริศ ศรตีงกิม")
+st.subheader('My sub')
